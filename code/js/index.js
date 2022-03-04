4f87d3ee4427fcea893c34644f6a1fd60ec07337
ekfaktldk11 <ekfaktldk11@naver.com>
2022-03-04 17:45:02 +0900

    modify index

--- code/js/index.js
+++ code/js/index.js
@@ -1,5 +1,54 @@
 let baseUrl = 'http://localhost:3001/';
 
+const imgCardStyling = () => {
+  document.getElementsByClassName('img-card').style = 'display : flex;position: relative;margin: 10px;width: 250px;height: 200px;z-index: 1;';
+  document.getElementsByClassName('space').style = 'width: 50px;';
+  document.getElementsByClassName('loaded-imgs').style = 'position: absolute;left: 50px;width: 200px;height: 200px;z-index: 2;object-fit: scale-down;';
+  document.getElementsByClassName('hidden-space').style = 'position: absolute;left: 200px;width: 50px;display: flex;flex-direction: column;height: 100%;';
+  document.getElementsByClassName('moving-block').addEventListener("mouseover", () => {
+    console.log(this);
+  })
+  document.getElementsByClassName('hidden-div').style = 'display: flex;flex: 1;justify-content: center;align-items: center;margin-left: 5px;';
+  // document.getElementsByClassName
+};
+
+const imgCard = (item, idx) => {
+  let trashIcon = 'fa-solid fa-trash fa-2xl';
+  let extenseIcon = 'fa-solid fa-up-right-and-down-left-from-center fa-2xl';
+  card.addEventListener
+  let card = document.createElement('div');
+  let space = document.createElement('div');
+  let movingBlock = document.createElement('div');
+  let img = document.createElement('img');
+  let hiddenSpace = document.createElement('div');
+  let hiddenDiv1 = document.createElement('div');
+  let hiddenDiv2 = document.createElement('div');
+  let hiddenIcon1 = document.createElement('i');
+  let hiddenIcon2 = document.createElement('i');
+
+  hiddenIcon1.className = trashIcon;
+  hiddenIcon2.className = extenseIcon;
+  hiddenDiv1.appendChild(hiddenIcon1);
+  hiddenDiv2.appendChild(hiddenIcon2);
+  hiddenSpace.append(hiddenDiv1, hiddenDiv2); // 이거 되나?
+
+  img.src = URL.createObjectURL(item);
+  img.alt = `loaded image num ${idx + 1}`;
+
+  movingBlock.append(img, hiddenSpace);
+  card.append(space, movingBlock);
+  
+  hiddenDiv1.className = 'hidden-div';
+  hiddenDiv2.className = 'hidden-div';
+  img.className = 'loaded-imgs'
+  card.className = 'img-card';
+  space.className = 'space';
+  movingBlock.className = 'moving-block';
+  hiddenSpace.className = 'hidden-space';
+
+  return card;
+};
+
 // 이미지 개수 받아오기
 const getFileLen = async (yy, mm) => {
   const response = await fetch(baseUrl + 'num-of-files', {
@@ -68,12 +117,13 @@ const loadBtnHandler = async (id) => {
           'margin: 0 auto;height: 130px;width: 90vw;white-space: nowrap;overflow-x: scroll;display: block;';
         for (let i = 0; i < fileLen; i++) {
           getFiles(i).then((result) => {
-            let img = document.createElement('img');
-            img.style =
-              'height: 100px;width: 200px;object-fit: scale-down;display: inline-block;';
-            img.src = URL.createObjectURL(result);
-            img.alt = `loaded image num ${i + 1}`;
-            div.appendChild(img);
+            div.appendChild(imgCard(result, i));
+            // let img = document.createElement('img');
+            // img.style =
+            //   'height: 100px;width: 200px;object-fit: scale-down;display: inline-block;';
+            // img.src = URL.createObjectURL(result);
+            // img.alt = `loaded image num ${i + 1}`;
+            // div.appendChild(img);
           });
         }
       } else {
@@ -83,6 +133,7 @@ const loadBtnHandler = async (id) => {
       }
     })
     .then(() => {
+      imgCardStyling();
       document.body.append(div);
     });
 };
@@ -91,8 +142,6 @@ const uploadFile = (id) => {
   console.log(document.getElementById(id).attributes);
   let ym = document.getElementById('calendar').value;
   ym = ym.split('-');
-}
+};
 
-const editModeEnable = () => {
-
-}
+const editModeEnable = () => {};
